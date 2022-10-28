d81d299184c97d35e57ba3f704a3fafc954d9abf
Shano1993 <stefan.hanotiau@gmail.com>
2022-10-28 14:44:18 +0200

    menu

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,1 @@
 let navBar = document.querySelector("#navBar");
-
-navBar.addEventListener("mouseenter", function () {
-    navBar.style.overflowX = "hidden"
-    navBar.style.overflowY = "scroll";
-})
-
-navBar.addEventListener("mouseout", function () {
-    navBar.style.overflowX = "hidden"
-    navBar.style.overflowY = "hidden";
-})
